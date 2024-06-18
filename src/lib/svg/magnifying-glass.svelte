7adba7e4de21dfc2ea90ead30f0fa075fe7b7e3a
Murray Champernowne <murrayadcock@gmail.com>
2024-06-18 16:30:21 +0100

    feat: svgs

---
+++ src/lib/svg/magnifying-glass.svelte
@@ -0,0 +1,4 @@
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="9.5" cy="9.5" r="6.5" stroke="currentColor" stroke-width="2"></circle>
+	<path d="M14 14L21 21" stroke="currentColor" stroke-width="2"></path>
+</svg>
