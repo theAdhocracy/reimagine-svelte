b201f9cfbe32becd227809051b1a47b2b5e13e2d
Murray Champernowne <murrayadcock@gmail.com>
2024-06-19 14:31:28 +0100

    feat: cta

---
+++ src/lib/components/CTA.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let url: string;
+	export let label: string;
+	export let type: 'highlight' | 'inverted' | undefined;
+</script>
+
+<a href={url || '/'} class={type ? `cta-${type}` : ''}>{label}</a>
+
+<style>
+	a,
+	button {
+		--border-color: var(--c-border);
+		--bg-color: transparent;
+		--text-color: var(--c-text);
+		--active-bg-color: var(--c-bg-page);
+		--lighten: 10%;
+
+		display: inline-block;
+		width: max-content;
+		height: fit-content;
+		padding: 0.5rem 1.5rem;
+		border: 1px solid var(--border-color);
+		background-color: var(--bg-color);
+		color: var(--text-color);
+		text-decoration: none;
+		font-size: 0.9375rem;
+		line-height: 2.4;
+		cursor: pointer;
+		text-align: center;
+		white-space: nowrap;
+
+		&:hover,
+		&:focus-visible {
+			background-color: color-mix(in srgb, var(--active-bg-color), white var(--lighten));
+		}
+	}
+
+	.cta-highlight {
+		--border-color: var(--c-highlight);
+		--bg-color: var(--c-highlight);
+		--text-color: var(--c-text-invert);
+		--active-bg-color: var(--c-highlight);
+		--lighten: 50%;
+	}
+
+	.cta-inverted {
+		--border-color: var(--c-text-invert);
+		--bg-color: var(--c-text-invert);
+		--active-bg-color: var(--c-text-invert);
+
+		&:focus-visible {
+			outline-color: var(--c-text-invert);
+		}
+	}
+</style>
